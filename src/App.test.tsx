8c42fef05b167ad19dff6628935c25703eaa4af2
encredible <descartes131@gmail.com>
2021-08-22 16:29:17 +0900

    PageHeader 컴포넌트

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const history = createMemoryHistory();
+  history.push('/');
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  const linkElement = screen.getByText('할 일 목록');
   expect(linkElement).toBeInTheDocument();
 });
